e4c3ae0a0505332a83711fb212f5b76c8a401cd3
ericbreh <ericchuang94@gmail.com>
2025-03-05 16:30:28 -0800

    transistor object

--- main.py
+++ main.py
@@ -17,6 +17,26 @@ E {}
 p_value = 0
 
 
+class Transistor:
+    def __init__(self, spice_line: str):
+        items = spice_line.split(" ")
+        self.length = items[-1]
+        self.width = items[-2]
+        self.name = items[-3].split("__")
+        self.body = items[-4]
+        self.drain = items[-5]
+        self.gate = items[-6]
+        self.source = items[-7]
+
+        # Normalize VPWR/VGND connections
+        if self.drain == "VPWR" or self.source == "VGND":
+            self.drain, self.source = self.source, self.drain
+
+    @property
+    def is_pmos(self) -> bool:
+        return self.name[1].startswith("p")
+
+
 class Point(NamedTuple):
     x: int
     y: int
@@ -82,101 +102,79 @@ def create_io_block(pins: Tuple[List[str], List[str]], origin: Point) -> str:
     return output
 
 
-def find_content(file: List[str]) -> List[str]:
+def find_content(file: List[str]) -> List[Transistor]:
     start = 0
     for index, line in enumerate(file):
         line = line.strip()
         if line.lower().startswith(".subckt"):
             start = index
         elif line.lower().startswith(".ends"):
-            return file[start + 1 : index]
+            return [Transistor(line) for line in file[start + 1: index]]
 
     raise ValueError("Invalid format")
 
-
-def is_pmos(item: str) -> bool:
     items = item.split(" ")
     transistor_name = items[-3].split("__")[1]
     return transistor_name.startswith("p")
 
 
 def create_single_transistor(
-    item: str, pos: Point, index: int, num_pmos: int = 0
+    transistor: Transistor, pos: Point, index: int, num_pmos: int = 0
 ) -> str:
     global p_value
     output = ""
 
-    item_list = item.split(" ")
-    transistor_length = item_list.pop()
-    transistor_width = item_list.pop()
-    transistor_name = item_list.pop().split("__")
+    output += f"C {{{'/'.join(transistor.name)}.sym}} {pos.x} {pos.y} 0 0 {{name=M{index + num_pmos}\nW={transistor.width[2:]}\nL={transistor.length[2:]}\nmodel={transistor.name[1]}\nspiceprefix=X\n}}\n"
 
-    transistor_body = item_list.pop()
-    transistor_drain = item_list.pop()
-    transistor_gate = item_list.pop()
-    transistor_source = item_list.pop()
-
-    if transistor_drain == "VPWR" or transistor_source == "VGND":
-        transistor_drain, transistor_source = transistor_source, transistor_drain
-
-    output += f"C {{{'/'.join(transistor_name)}.sym}} {pos.x} {pos.y} 0 0 {{name=M{index + num_pmos}\nW={transistor_width[2:]}\nL={transistor_length[2:]}\nmodel={transistor_name[1]}\nspiceprefix=X\n}}\n"
-
-    output += f"C {{lab_pin.sym}} {pos.x + 20} {pos.y} 2 0 {{name=p{p_value} sig_type=std_logic lab={transistor_body}}}\n"
+    output += f"C {{lab_pin.sym}} {pos.x + 20} {pos.y} 2 0 {{name=p{p_value} sig_type=std_logic lab={transistor.body}}}\n"
     p_value += 1
 
-    output += f"C {{lab_pin.sym}} {pos.x + 20} {pos.y - 30} 2 0 {{name=p{p_value} sig_type=std_logic lab={transistor_source}}}\n"
+    output += f"C {{lab_pin.sym}} {pos.x + 20} {pos.y - 30} 2 0 {{name=p{p_value} sig_type=std_logic lab={transistor.source}}}\n"
     p_value += 1
 
-    output += f"C {{lab_pin.sym}} {pos.x + 20} {pos.y + 30} 2 0 {{name=p{p_value} sig_type=std_logic lab={transistor_drain}}}\n"
+    output += f"C {{lab_pin.sym}} {pos.x + 20} {pos.y + 30} 2 0 {{name=p{p_value} sig_type=std_logic lab={transistor.drain}}}\n"
     p_value += 1
 
-    output += f"C {{lab_pin.sym}} {pos.x - 20} {pos.y} 0 0 {{name=p{p_value} sig_type=std_logic lab={transistor_gate}}}\n"
+    output += f"C {{lab_pin.sym}} {pos.x - 20} {pos.y} 0 0 {{name=p{p_value} sig_type=std_logic lab={transistor.gate}}}\n"
     p_value += 1
 
     return output
 
 
-def create_transistors(items: List[str], origin: Point) -> str:
+def create_transistors(transistors: List[Transistor], origin: Point) -> str:
     pmos_transistors = []
     nmos_transistors = []
 
-    for item in items:
-        if is_pmos(item):
-            pmos_transistors.append(item)
+    for trans in transistors:
+        if trans.is_pmos:
+            pmos_transistors.append(trans)
         else:
-            nmos_transistors.append(item)
+            nmos_transistors.append(trans)
 
     output = ""
-    # pmos_width = math.floor(math.sqrt(len(pmos_transistors))) or 1
-    for index, item in enumerate(pmos_transistors):
+    for index, trans in enumerate(pmos_transistors):
         pos = Point(
-            # origin.x + ((index % pmos_width) * 280),
-            # origin.y + ((index // pmos_width) * 180),
             origin.x + (index * 120),
             origin.y
         )
-        output += create_single_transistor(item, pos, index)
+        output += create_single_transistor(trans, pos, index)
 
-    # nmos_width = math.floor(math.sqrt(len(nmos_transistors))) or 1
-    # nmos_origin = Point(origin.x, origin.y +
-    #                     (len(pmos_transistors) // pmos_width + 2) * 180)
     nmos_origin = Point(origin.x, origin.y + 280)
 
-    for index, item in enumerate(nmos_transistors):
+    for index, trans in enumerate(nmos_transistors):
         pos = Point(
-            # nmos_origin.x + ((index % nmos_width) * 280),
-            # nmos_origin.y + ((index // nmos_width) * 180),
             nmos_origin.x + (index * 120),
             nmos_origin.y
         )
         output += create_single_transistor(
-            item, pos, index, len(pmos_transistors))
+            trans, pos, index, len(pmos_transistors))
 
     return output
 
 
 def main() -> None:
-    parser = ArgumentParser(description="Copy the contents of one file to another.")
+    parser = ArgumentParser(
+        description="Copy the contents of one file to another.")
     parser.add_argument(
         "-v",
         "--version",
